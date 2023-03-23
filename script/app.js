227ea6e53a59edf818b514a691b7c0df4c87773d
ColiJD <colindresj9@gmail.com>
2023-03-23 09:51:52 -0600

    Botonn de whatsApp

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@ import { validartxt } from "./validar.js";
 
 const inputs = document.querySelectorAll('input');
 const textarea = document.querySelectorAll('textarea')
+const whatsapp =  document.querySelector('[data-whatsapp]')
 
 inputs.forEach(input => {
     input.addEventListener('blur', (input) => {
@@ -15,3 +16,11 @@ textarea.forEach(textarea => {
         validar(textarea.target);
     });
 });
+
+whatsapp.addEventListener('click', (event) => {
+    contactar(event.target);
+})
+
+function contactar(){
+    window.open("https://api.whatsapp.com/send?phone=+50488716380&text=Hola%21%20Quisiera%20chatear%20con%20usted.", "_blank");
+}
